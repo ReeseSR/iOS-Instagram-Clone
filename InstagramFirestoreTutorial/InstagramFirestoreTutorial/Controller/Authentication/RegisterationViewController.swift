224d83aafe9d10666e2e2ef06186e2d013670bc0
bsorinnn <bxxsxrxx@gmail.com>
2022-03-13 00:03:53 +0900

    [Feat] Login UI 작업
    - Authentication VIew 생성
    - Login UI 뷰 작업
    - rootController를 LoginVC로 설정

---
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/Authentication/RegisterationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterationViewController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by 배소린 on 2022/03/12.
+//
+
+import UIKit
+
+class RegisterationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
